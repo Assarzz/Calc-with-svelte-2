72aebb06484f40088097f9f86518e76c51bca348
Assar <assarlannerbornzz@gmail.com>
2023-09-20 12:18:22 +0200

    color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,29 @@
     import Display from "./display.svelte";
 import Keyboard from "./keyboard.svelte";
 
+let clicks = 0
+//let myObject
+
+function updateClicks(){
+    clicks ++
+}
+// function merge(mergeObj){
+//     myObject = [...myObject, ...mergeObj]
+// }
+
+// let accessorObj = {
+//     updateClicks: updateClicks,
+//     merge: merge
+// }
 </script>
 
 <div>
 
     <Display/>
-    <Keyboard/>
+    <Keyboard {clicks} {updateClicks} />
 
 </div>
-
+<h1>{clicks}</h1>
 <style>
 
     div{
--- src/routes/keyboard.svelte
+++ src/routes/keyboard.svelte
@@ -1,5 +1,5 @@
 <script>
-    let keyboard = [];
+    export let clicks, updateClicks
 
     let col1 = [
         { value: "1", clicked: false, type: "number"},
@@ -10,6 +10,8 @@
         { value: "2", clicked: false, type: "number"},
         { value: "5", clicked: false, type: "number"},
         { value: "8", clicked: false, type: "number"},
+        { value: "0", clicked: false, type: "number"},
+
     ]
     let col3 = [
         { value: "3", clicked: false, type: "number"},
@@ -25,41 +27,19 @@
         { value: "/", clicked: false, type: "operator"},
     ]
 
-    for (let index = 0; index < 10; index++) {
-    }
-    function getKey(getID){
+    const keyboard = [...col1, ...col2, ...col3, ...col4, ...col5];
 
-    }
-    keyboard.push({ value: "+", clicked: false, type: "operator", id:"+" });
-    keyboard.push({ value: "-", clicked: false, type: "operator", id:"-" });
-    keyboard.push({ value: "*", clicked: false, type: "operator", id:"*" });
-    keyboard.push({ value: "/", clicked: false, type: "operator", id:"/" });
     for (let index = 0; index < keyboard.length; index++) {
         const alt = ["red", "green", "blue"]
         keyboard[index]["color"] = (alt[Math.floor(Math.random()*3)]) 
         
     }
 
-    // function determine(index){
-    //     if (keyboard[index].type != "operator"){
-
-    //         if (index%3 == 0){
-    //             return "col2"
-    //         }
-    //         else if (index%3 == 1){
-    //             return "col1"
-    //         }
-    //         else{
-    //             return "col3"
-    //         }
-    //     }
-    //     else{
-    //         return "op"
-    //     }
-    // }
 
     function click(col, index){
-        console.log("clicked!")
+
+        updateClicks()
+
 
         col[index].clicked = true
 
@@ -71,73 +51,8 @@
     }
 </script>
 
-<!--
+
 <div id="keyboard">
-    <div id="col1">
-        <div class="key {keyboard[1].color}">
-            {keyboard[1].value}
-        </div>
-        <div class="key {keyboard[4].color}">
-            {keyboard[4].value}
-        </div>
-        <div class="key {keyboard[7].color}">
-            {keyboard[7].value}
-        </div>
-    </div>
-
-    <div id="col2">
-        <div class="key {keyboard[2].color}">
-            {keyboard[2].value}
-        </div>
-        <div class="key {keyboard[5].color}">
-            {keyboard[5].value}
-        </div>
-        <div class="key {keyboard[8].color}">
-            {keyboard[8].value}
-        </div>
-        <div class="key {keyboard[0].color}">
-            {keyboard[0].value}
-        </div>
-    </div>
-
-    <div id="col3">
-        <div class="key {keyboard[3].color}">
-            {keyboard[3].value}
-        </div>
-        <div class="key {keyboard[6].color}">
-            {keyboard[6].value}
-        </div>
-        <div class="key {keyboard[9].color}">
-            {keyboard[9].value}
-        </div>
-    </div>
-
-    <div id="col4">
-
-        <div class="key {keyboard[10].color} op">
-            {keyboard[10].value}
-        </div>
-        <div class="key {keyboard[11].color} op">
-            {keyboard[11].value}
-        </div>
-    </div>
-    <div id="col5">
-        <div class="key {keyboard[12].color} op">
-            {keyboard[12].value}
-        </div>
-        <div class="key {keyboard[13].color} op">
-            {keyboard[13].value}
-        </div>
-    </div>
-</div>
--->
-<div id="keyboard">
-
-    <!-- {#each keyboard as key, index }
-        <button class="key {key.color} {() =>determine(index)} {(key.clicked? "clicked": "")}" on:click={()=> click(index)}>
-            {key.value}
-        </button>
-    {/each} -->
 
     <div class="col1">
         {#each col1 as key, index}
@@ -178,6 +93,34 @@
 
 <style lang="scss">
     $def-border-radious: 10px;
+
+    #keyboard{
+        display: inline-flex;
+        background-color: rgba(0, 0, 0, 0.827);
+        div{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+
+            button{
+                border-radius: 100px;
+                width: 100px;
+                height: 100px;
+                margin: 5px;
+                color: white;
+                font-size: 5rem;
+                display: flex;
+                justify-content: center;
+
+                .clicked{
+                    transform: scale(0.9);
+                }
+
+            }
+
+        }
+
+    }
 
 
 
